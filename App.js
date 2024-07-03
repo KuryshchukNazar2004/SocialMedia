6cd37480570762a1bfb8118ef33fcf55be3ee282
Nazar Kuryshchuk <kuryshchuk@Nazars-Laptop.local>
2024-07-03 13:25:59 +0300

    first init

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import { getFontFamily } from './assets/fonts/helper';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '200')}}>
+        Hello World
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
